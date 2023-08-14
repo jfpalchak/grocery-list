c859d3a97d04954e33ae97832fc2de48a5a09544
Joey Palchak <joey.palchak@gmail.com>
2023-08-14 16:10:10 -0700

    Create event handlers for form submission, list creation, and form display.
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,37 @@
 function handleSubmit(e) {
     e.preventDefault();
 
+    const groceryItems = document.querySelectorAll("input[name=grocery-option]:checked");
+
+    const groceryItemsArray = Array.from(groceryItems);
+
+    // groceryItemsArray.sort();
+    const groceryArrayCapt = []
+    groceryItemsArray.forEach(function(element) {
+        groceryArrayCapt.push(element.value.toUpperCase());
+    });
+    
+    groceryArrayCapt.sort();
+
+    const ul = document.createElement("ul");
+    document.body.append(ul);
+
+    groceryArrayCapt.forEach(function(itemsList) {
+        let li = document.createElement("li");
+        li.append(itemsList);
+        ul.append(li);
+    });
+
+    document.querySelector('form').setAttribute('class', 'hidden');
+    // let form = document.querySelector('form');
+    // form.remove();
     
 
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
 
 function handleEverything() {
     const form = document.querySelector('form');
